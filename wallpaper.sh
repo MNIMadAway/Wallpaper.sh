03df8a259035f1edf9d1f97281ed071c6a09a336
Mad Away <122818426+MNIMadAway@users.noreply.github.com>
2023-11-06 17:17:21 +0300

    Update wallpaper.sh

--- wallpaper.sh
+++ wallpaper.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
 #Current dir variable
 	curdir=$(dirname $(readlink -e "$0"))
-	dir = /home/dcadmin/wallpaper
-	su = echo 1 | sudo -S -u dcadmin
+	newdir = /home/dcadmin/wallpaper
+	#su = echo 1 | sudo -S -u dcadmin
 	#
 	#cd $curdir
 	#echo $pic
 
 #Change wallpaper
-	mkdir $dir
-	mv $curdir/wall $dir/wall
-	gsettings set org.gnome.desktop.background picture-uri "file://\$dir/wall"
+	mkdir $newdir
+	mv $curdir/wall $newdir/wall
+	gsettings set org.gnome.desktop.background picture-uri "file://\$newdir/wall"
 
 #Change background
 	cd $curdir
 	wget -q https://raw.githubusercontent.com/PRATAP-KUMAR/ubuntu-gdm-set-background/main/ubuntu-gdm-set-background && chmod +x ubuntu-gdm-set-background
 	sudo apt-get install libglib2.0-dev-bin -y
-	sudo $curdir/ubuntu-gdm-set-background --image $dir/wall
+	sudo $curdir/ubuntu-gdm-set-background --image $newdir/wall
 
 #
 #reboot -i
